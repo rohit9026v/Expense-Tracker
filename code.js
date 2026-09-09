84ed854c01daf539bd352ae0b95e95e54b74836c
Rohit verma <rohit9026v@gmail.com>
2026-09-09 14:41:23 +0530

    Moved from static card to dynamic expense cards

--- code.js
+++ code.js
@@ -6,7 +6,7 @@ let addButton = document.querySelector("#add-btn");
 let tAmount = document.querySelector(".tamount");
 let tEntries = document.querySelector(".tentries");
 let tMonth = document.querySelector(".tmonth");
-let filCategory = document.querySelector("#fil-category");
+let categoryFilt = document.querySelector("#fil-category");
 let disWrapper = document.querySelector(".dis-wrapper");
 
 //date and time Updation
@@ -50,9 +50,10 @@ addButton.addEventListener("click", () => {
 
   localStorage.setItem("expense", JSON.stringify(expenseArr));
   totalExpdis();
-  disExpense();
   totalEntries();
   monthTExp();
+  renderCategories();
+  renderCategoryDis();
   inpAmount.value = "";
   inpTitle.value = "";
 });
@@ -95,53 +96,117 @@ function monthTExp() {
 }
 monthTExp();
 
-//category filter
+//dynamic filter option
+
+function renderCategories() {
+  categoryFilt.innerHTML = "";
+
+  const item = document.createElement("option");
+  item.innerText = "All Category";
+  item.value = "all-category";
+  categoryFilt.appendChild(item);
+
+  const list = getExpense().map((item) => {
+    return item.category;
+  });
+
+  const categories = [...new Set([...list])].sort();
+
+  for (let i = 0; i < categories.length; i++) {
+    const item = document.createElement("option");
+    item.innerText = categories[i];
+    item.value = categories[i];
+    categoryFilt.appendChild(item);
+  }
+}
+
+renderCategories();
+
+//applying filter
 
 let category = document.querySelectorAll(".exp-dis");
-filCategory.addEventListener("change", () => {
+categoryFilt.addEventListener("change", () => {
   category.forEach((element) => {
     element.classList.remove("hidden");
     if (
-      filCategory.value !== "all-category" &&
-      !element.classList.contains(filCategory.value)
+      categoryFilt.value !== "all-category" &&
+      !element.classList.contains(categoryFilt.value)
     ) {
       element.classList.add("hidden");
     }
   });
 });
 
-//category wise expense display
+//dynamic category display
 
-function disExpense() {
-  let categoryExp = document.querySelectorAll(".exp");
-  let expenseArr = getExpense();
-  categoryExp.forEach((item) => {
-    let id = item.getAttribute("id");
-    let disCategoryAmt = document.querySelector(`#${id}`);
-    let categoryTime = disCategoryAmt.closest(".exp-dt").querySelector(".d-t");
-    let bal = 0;
-    let found = false;
+function renderCategoryDis() {
+  disWrapper.innerHTML = "";
+  const list = getExpense().map((item) => {
+    return item.category;
+  });
 
-    expenseArr.forEach((element) => {
-      if (element.category == id) {
-        found = true;
-
-        //Amount Update
-        bal += Number(element.expense);
-        disCategoryAmt.innerText = `₹ ${bal}`;
-
-        //Time Update
-        categoryTime.innerText = element.time;
+  const categories = [...new Set([...list])].sort();
+  categories.forEach((item) => {
+    let time;
+    const finalAmt = getExpense().reduce((acc, current) => {
+      if (current.category == item) {
+        acc += Number(current.expense);
+        time = current.time;
       }
-    });
-    if (!found) {
-      disCategoryAmt.innerText = "";
-      categoryTime.innerText = "";
-    }
+      return acc;
+    }, 0);
+
+    const wrapperDiv = document.createElement("div");
+    wrapperDiv.className = `exp-dis ${item}`; //yha ek aur class add krni hai category ki  Done
+
+    const categoryDiv = document.createElement("div"); //child 1
+    categoryDiv.className = "category";
+    const icon = document.createElement("img"); // image tag created
+    icon.className = "category-icon";
+    icon.alt = "category-icon";
+    icon.src = `./assets/${item}.png`; //dynamic images
+    categoryDiv.appendChild(icon); //sub-child 1
+    const para = document.createElement("p"); //p tag created
+    para.innerText = item.toUpperCase(); //dynamic name
+    categoryDiv.appendChild(para); //sub-child 2 appended
+    wrapperDiv.appendChild(categoryDiv); //child 1 appended
+
+    const categoryAmtDiv = document.createElement("div"); //child 2
+    categoryAmtDiv.className = "category-amount";
+
+    const expenseDetails = document.createElement("div"); //sub-child 1
+    expenseDetails.className = "exp-dt";
+    const amtPara = document.createElement("p");
+    amtPara.className = "exp amount";
+    amtPara.innerText = `₹ ${finalAmt}`;
+    amtPara.id = item; //dynamic id
+    expenseDetails.appendChild(amtPara);
+    const span = document.createElement("span");
+    span.className = "d-t";
+    span.id = `${item}-time`;
+    span.innerText = time;
+    expenseDetails.appendChild(span);
+    categoryAmtDiv.appendChild(expenseDetails); // sub child 1 appended
+
+    const deleteDiv = document.createElement("div");
+    deleteDiv.className = "delete";
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "delete-btn";
+    deleteButton.type = "button";
+    const buttonImg = document.createElement("img");
+    buttonImg.className = "delete-img";
+    buttonImg.src = "./assets/delete.png";
+    deleteButton.appendChild(buttonImg);
+    deleteDiv.appendChild(deleteButton);
+
+    categoryAmtDiv.appendChild(deleteDiv); //sub-child 2 appended
+    wrapperDiv.appendChild(categoryAmtDiv); // child 2 appended
+
+    disWrapper.appendChild(wrapperDiv); //appended to a common wrapper
   });
 }
 
-disExpense();
+renderCategoryDis();
 
 //category wise delete button
 
@@ -153,7 +218,7 @@ disWrapper.addEventListener("click", (e) => {
     return;
   }
 
-  if (confirm("Are you sure you want to delete this expense")) {
+  if (confirm("Are you sure you want to delete this entire expense")) {
     let amount = e.target.closest(".category-amount").querySelector(".exp");
     let id = amount.getAttribute("id");
     let updatedArr = getExpense().filter((item) => item.category !== id);
@@ -162,8 +227,9 @@ disWrapper.addEventListener("click", (e) => {
     //Update amount displayed
 
     totalExpdis();
-    disExpense();
     totalEntries();
     monthTExp();
+    renderCategories();
+    renderCategoryDis();
   }
 });
